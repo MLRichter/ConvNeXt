3adbfdb8692bda021fa3a56a1e7644b29dce146e
MLRichter <matrichter@uos.de>
2022-10-04 16:11:14 -0400

    added improved convnext

--- datasets.py
+++ datasets.py
@@ -57,11 +57,12 @@ def build_dataset(is_train, args):
             with tarfile.open(args.data_path) as tf:  # cannot keep this open across processes, reopen later
                 train = ParserImageTar(args.data_path, tf=tf, subset="train")
                 val = ParserImageTar(args.data_path, tf=tf, subset="val")
-                train_data = ImageDataset(root=args.data_path, reader=train)
-                val_data = ImageDataset(root=args.data_path, reader=val)
-                imnet21k_cache["train"] = train_data
-                imnet21k_cache["val"] = val_data
-        dataset = imnet21k_cache["train"] if is_train else imnet21k_cache["val"]
+                imnet21k_cache["train"] = train
+                imnet21k_cache["val"] = val
+
+        dataset = ImageDataset(root=args.data_path,
+                                reader=imnet21k_cache["train"] if is_train else imnet21k_cache["val"],
+                                transform=transform)
         nb_classes = 10450
     elif args.data_set == "image_folder":
         root = args.data_path if is_train else args.eval_data_path
