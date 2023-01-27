50bb1556a8691f022da8a7d0ac20d52ea83b0e9d
MLRichter <matrichter@uos.de>
2023-01-27 11:23:35 -0500

    updated downsampling

--- object_detection/mmdet/models/backbones/convnext.py
+++ object_detection/mmdet/models/backbones/convnext.py
@@ -70,12 +70,13 @@ class ConvNeXt(nn.Module):
     """
     def __init__(self, in_chans=3, depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], 
                  drop_path_rate=0., layer_scale_init_value=1e-6, out_indices=[0, 1, 2, 3],
+                 stem=4
                  ):
         super().__init__()
 
         self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers
         stem = nn.Sequential(
-            nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
+            nn.Conv2d(in_chans, dims[0], kernel_size=stem, stride=stem),
             LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
         )
         self.downsample_layers.append(stem)
